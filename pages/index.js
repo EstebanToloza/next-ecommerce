fa910bf5994a0721395e1efe3aa0e570f1ad8eed
EstebanToloza <estebanisaiastoloza@gmail.com>
2022-01-14 12:10:39 -0300

    dynamic product url and product page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, Typography } from '@material-ui/core'
 import Head from 'next/head'
+import NextLink from 'next/link'
 import Image from 'next/image'
 import Layout from '../components/Layout'
-import data from '../utils/data'
+import data from '../utils/data';
 
 export default function Home() {
   return (
@@ -10,28 +11,33 @@ export default function Home() {
       <div>
         <h1>Products</h1>
         <Grid container spacing={3}>
-          {data.products.map((product) => (
-            <Grid item md={4} key={product.name}>
-              <Card>
-                <CardActionArea>
-                  <CardMedia 
-                    component="img" 
-                    image={product.image} 
-                    title={product.name}
-                  />
-                  <CardContent>
-                    <Typography>
-                      {product.name}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-                <CardActions>
-                  <Typography>$ {product.price}</Typography>
-                  <Button size='small' color='primary'>Add to cart</Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
+          {data.products.map((product) => {
+            //const slug = product.name.toLowerCase().replaceAll(' ', '-');
+            return (
+              <Grid item md={4} key={product.name}>
+                <Card>
+                  <NextLink href={`/product/${product.slug}`} passHref>
+                    <CardActionArea>
+                      <CardMedia 
+                        component="img" 
+                        image={product.image} 
+                        title={product.name}
+                      />
+                      <CardContent>
+                        <Typography>
+                          {product.name}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </NextLink>
+                  <CardActions>
+                    <Typography>$ {product.price}</Typography>
+                    <Button size='small' color='primary'>Add to cart</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            )
+          })}
         </Grid>
       </div>
     </Layout>
