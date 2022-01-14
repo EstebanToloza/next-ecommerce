1a72f3a9a060ab66ee837bec6b1dc3c379667344
EstebanToloza <estebanisaiastoloza@gmail.com>
2022-01-14 10:15:28 -0300

    header styles

--- components/Layout.js
+++ components/Layout.js
@@ -1,24 +1,27 @@
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
 import Head from 'next/head';
 import React from 'react';
+import useStyles from '../utils/utils';
 
 const Layout = ({children}) => {
     const date = new Date().getFullYear();
+
+    const classes = useStyles();
 
     return (
         <div>
             <Head>
                 <title>Next ecommerce</title>
             </Head>
-            <AppBar position='static'>
+            <AppBar position='static' className={classes.navBar}>
                 <Toolbar>
                     <Typography>Next Ecommerce</Typography>
                 </Toolbar>
             </AppBar>
-            <Container>
+            <Container className={classes.main}>
                 {children}
             </Container>
-            <footer>
+            <footer className={classes.footer}>
                 <Typography>
                     &copy; {`All rights reserved. Next Eccommerce. ${date}`}
                 </Typography>
